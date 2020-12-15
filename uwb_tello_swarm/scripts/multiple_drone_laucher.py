a65f0027daa47682fe9f5410b5f83ccbb565bdb6
Jorge <jopequ@utu.fi>
2020-12-15 17:27:26 +0200

    fix port and change h264 default

--- uwb_tello_swarm/scripts/multiple_drone_laucher.py
+++ uwb_tello_swarm/scripts/multiple_drone_laucher.py
@@ -1,27 +1,39 @@
 #! /usr/bin/python
 
-import roslaunch
-import rospy
+import sys
 import time
+import rospy
+import roslaunch
 
-# <arg name="namespace"			default="tello" />
-# <arg name="tello_ip" 			default="192.168.10.1" />
-# <arg name="local_cmd_client_port"	default="8890" />
-# <arg name="local_vid_server_port"	default="6038" />
-# <arg name="tello_cmd_server_port"	default="8889" />
+'''
+   For reference:
+      <arg name="namespace"			default="tello" />
+      <arg name="tello_ip" 			default="192.168.10.1" />
+      <arg name="local_cmd_client_port"	default="8890" />
+      <arg name="local_vid_server_port"	default="6038" />
+      <arg name="tello_cmd_server_port"	default="8889" />
+'''
+
+# class SwarmLauncher() :
+
+#    def __init__(self) :
 
 tello_ip_start = 150
 tello_port = 8889
 
 local_port_start = 8890
-local_vid_port_start = 6028   # NOT WORKING ATM
+local_vid_port_start = 6038   # NOT WORKING ATM
 
 '''
    Number of nodes (IPs considered consecutive)
    and list of matching UWB active tags
 '''
-num_nodes = 3
-
+num_nodes = 1
+if len(sys.argv) > 0 :
+   try : 
+      num_nodes = int(sys.argv[1])
+   except :
+      print("Cannot read number of drones (sys.argv[1]). Setting num_nodes=1...")
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
@@ -41,7 +53,7 @@ for num in range(num_nodes):
                'tello_ip:=192.168.1.{}'.format(tello_ip_start + num), 
                'local_cmd_client_port:={}'.format(local_port_start + num),
                'local_vid_server_port:={}'.format(local_vid_port_start + num), 
-               'tello_cmd_server_port:={}'.format(tello_port + num)
+               'tello_cmd_server_port:={}'.format(tello_port)
             ]
    roslaunch_args = cli_args[2:]
    roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
@@ -53,4 +65,11 @@ for num in range(num_nodes):
 
    parent.start()
 
-rospy.spin()
+
+try :
+   while not rospy.is_shutdown() :
+      time.sleep(0.1)
+except :
+   print("Exiting...")
+   pass
+
