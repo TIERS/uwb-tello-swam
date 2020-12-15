af8914ff99f2a32ac7be4912853aa99615799695
Jorge <jopequ@utu.fi>
2020-12-15 15:05:57 +0200

    initial version, single objective, proof of concept

--- uwb_tello_swarm/scripts/multiple_drone_laucher.py
+++ uwb_tello_swarm/scripts/multiple_drone_laucher.py
@@ -10,17 +10,39 @@ import time
 # <arg name="local_vid_server_port"	default="6038" />
 # <arg name="tello_cmd_server_port"	default="8889" />
 
+tello_ip_start = 150
+tello_port = 8889
+
+local_port_start = 8890
+local_vid_port_start = 6028   # NOT WORKING ATM
+
+'''
+   Number of nodes (IPs considered consecutive)
+   and list of matching UWB active tags
+'''
 num_nodes = 3
+
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 
 launch_file = "tello_node_multi.launch"
 launch_files = []
+
+'''
+
+   Launch Tello node for each
+
+'''
 for num in range(num_nodes):
-   cli_args = ['tello_driver', launch_file, 'namespace:=tello{}'.format(num), 
-               'tello_ip:=192.168.1.{}'.format(150+num), 'local_cmd_client_port:={}'.format(8890+num),
-               'local_vid_server_port:={}'.format(6038 + num), 'tello_cmd_server_port:={}'.format(8889)]
+   cli_args = ['tello_driver', 
+               launch_file, 
+               'namespace:=tello{}'.format(num), 
+               'tello_ip:=192.168.1.{}'.format(tello_ip_start + num), 
+               'local_cmd_client_port:={}'.format(local_port_start + num),
+               'local_vid_server_port:={}'.format(local_vid_port_start + num), 
+               'tello_cmd_server_port:={}'.format(tello_port + num)
+            ]
    roslaunch_args = cli_args[2:]
    roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
    print(cli_args, " ===>>>  ", num)
